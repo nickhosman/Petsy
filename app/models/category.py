cf75f610f518fa23c9760fe09c49eef6966e4f32
heehyun1128 <chenyiapp@yahoo.com>
2023-09-22 16:05:17 -0700

    category model

--- app/models/category.py
+++ app/models/category.py
@@ -1,1 +1,22 @@
 # __tablename__ = "categories"
+from .db import db, environment, SCHEMA
+
+
+
+class Category(db.Model):
+  __tablename__ = "categories"
+
+  if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(255), nullable=False)
+
+  # relationships
+  products=db.relationship("Product",back_populates="category")
+
+  def to_dict(self):
+    return {
+      "id":self.id,
+      "name":self.name,
+    }
