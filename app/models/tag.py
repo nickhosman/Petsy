05d9164f375775a796e55f8350fc7e8108cb83fc
Huynh <huynhlam56@gmail.com>
2023-09-22 16:04:45 -0700

    add tags table/model

--- app/models/tag.py
+++ app/models/tag.py
@@ -1,1 +1,19 @@
 # "tags"
+from .db import db, environment, SCHEMA
+
+class Tag(db.Model):
+  __tablename__ = 'tags'
+
+  if environment == "production":
+      __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(255), nullable=False, unique=True)
+
+  products = db.relationship('Product', back_populates='all_tags')
+
+  def to_dict(self):
+     return {
+        "id": self.id,
+        "name": self.name
+     }
