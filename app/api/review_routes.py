51badabbd5bad6f3fcc2806cd29b17b00efd57b7
Nick Hosman <nickhosman@gmail.com>
2023-09-23 12:16:27 -0700

    finished delete review by id route

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,5 +1,28 @@
 from flask import Blueprint
 from app.models import Review, db
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 review_routes = Blueprint("reviews", __name__)
+
+@review_routes.route("/<int:reviewId>", methods=["DELETE"])
+@login_required
+def delete_review(reviewId):
+  """
+  Deletes a review from the database by id
+  """
+  review = Review.query.get(reviewId)
+  if review.user_id == current_user.id:
+    db.session.delete(review)
+    db.session.commit()
+  return {'errors': ['Unauthorized']}
+
+
+@review_routes.route("/<int:reviewId>", methods=["PUT"])
+@login_required
+def edit_review(reviewId):
+  """
+  Updates a review in the database by id
+  """
+  review = Review.query.get(reviewId)
+  if review.user_id == current_user.id:
+    
