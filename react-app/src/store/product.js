da59d42cda0531234a04576ccd12bc5e67628fbb
heehyun1128 <chenyiapp@yahoo.com>
2023-09-24 22:22:45 -0700

    working on redux store for products

--- react-app/src/store/product.js
+++ react-app/src/store/product.js
@@ -1,13 +1,33 @@
+/** Action Type Constants: */
+export const LOAD_PRODUCTS = 'products/LOAD_PRODUCTS'
+export const GET_PRODUCT = 'products/GET_PRODUCT'
+export const EDIT_PRODUCT = 'products/EDIT_PRODUCT'
+export const REMOVE_PRODUCT = 'products/REMOVE_PRODUCT'
 
-const LOAD_PRODUCTS = 'products/loadSpots'
-
-const loadProducts = (products) => {
+/**  Action Creators: */
+export const loadProducts = (products) => {
   return {
     type: LOAD_PRODUCTS,
     products
   }
 };
 
+export const getProduct = product =>({
+  type:GET_PRODUCT,
+  product
+})
+
+export const editProduct = product =>({
+  type:EDIT_PRODUCT,
+  product
+})
+
+export const removeProduct = productId =>({
+  type:REMOVE_PRODUCT,
+  productId
+})
+
+/** Thunk Action Creators: */
 export const thunkLoadProducts = () => async(dispatch) => {
   const response = await fetch('/api/products')
   if(response.ok) {
@@ -20,6 +40,56 @@ export const thunkLoadProducts = () => async(dispatch) => {
   }
 };
 
+
+export const fetchProductDetail = productId =>async(dispatch)=>{
+  const response = await fetch('/api/products/:productid')
+  if(response.ok){
+    const productDetails= await response.json()
+    dispatch(getProduct(productDetails))
+    return productDetails
+  }else{
+    let errors = await response.json()
+    return errors
+  }
+}
+
+
+export const createProduct = product => async (dispatch) =>{
+  const response = await fetch("/api/products/new",{
+    method:"POST",
+    headers: { 'Content-Type': 'application/json' },
+    body:JSON.stringify(product)
+  })
+
+  if (response.ok) {
+    const newProduct = await response.json()
+    dispatch(getProduct(newProduct))
+    return newProduct
+  } else {
+    let errors = await response.json()
+    return errors
+  }
+}
+
+export const updateProduct = product => async (dispatch) =>{
+  const response = await fetch(`/api/products/${product.id}`,{
+    method:"POST",
+    headers: { 'Content-Type': 'application/json' },
+    body:JSON.stringify(product)
+  })
+
+  if (response.ok) {
+    const newProduct = await response.json()
+    dispatch(getProduct(newProduct))
+    return newProduct
+  } else {
+    let errors = await response.json()
+    return errors
+  }
+}
+
+
+// product reducer
 const initialState = {}
 const productReducer = (state = initialState, action) => {
   let newState;
