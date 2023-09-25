664f2de08a9e424a10597072b5fde5b043977962
heehyun1128 <chenyiapp@yahoo.com>
2023-09-25 12:52:22 -0700

    ..

--- react-app/src/components/Product/ProductCard/index.js
+++ react-app/src/components/Product/ProductCard/index.js
@@ -9,7 +9,7 @@ function ProductCard({ product }) {
   const history = useHistory()
   const handleViewProductDetail = e => {
     e.preventDefault()
-    history.push(`/api/products/${product.id}`)
+    history.push(`/products/${product.id}`)
   }
   return (
     <div className="k-productcard-container" onClick={handleViewProductDetail}>
