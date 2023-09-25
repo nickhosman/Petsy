778c2cc7f9b5a97098d51693984e7efc0412693e
heehyun1128 <chenyiapp@yahoo.com>
2023-09-25 12:47:06 -0700

    useHistory at productCard

--- react-app/src/components/Product/ProductCard/index.js
+++ react-app/src/components/Product/ProductCard/index.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router-dom'
 import './ProductCard.css'
 
 function ProductCard({ product }) {
-  return (
-    <div className="k-productcard-container">
-      <NavLink style={{ textDecoration: "none", color: "black" }} to={`/products/${product.id}`}>
-        <div>
-          <img className='k-productcard-image' src={product.previewImage} alt=''></img>
-        </div>
-        <div>
-          <h4>{product.name}</h4>
-          <p>{product.averageRating}</p>
 
-          <p>${product.price}</p>
+  // console.log(product)
+  const history = useHistory()
+  const handleViewProductDetail = e => {
+    e.preventDefault()
+    history.push(`/api/products/${product.id}`)
+  }
+  return (
+    <div className="k-productcard-container" onClick={handleViewProductDetail}>
+      <div id="product-image-div">
+        <img className='k-productcard-image' src={product.previewImage} alt=''></img>
+      </div>
+      <div id="product-info-div">
+        <h4>{product.name}</h4>
+        <div id="product-rating-div">
+          <p>{product.averageRating}</p>
+          <i class="fa-solid fa-star"></i>
         </div>
-      </NavLink>
+        <p id='price'>${product.price}</p>
+      </div>
     </div>
   )
 }
