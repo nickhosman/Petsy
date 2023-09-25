5631d3d7a1b1387c30d78f7c17301dc268de880a
Kevin <kevindbaik@gmail.com>
2023-09-25 11:48:09 -0700

    basic functionality of product details complete

--- react-app/src/components/Product/ProductCard/index.js
+++ react-app/src/components/Product/ProductCard/index.js
@@ -1,29 +1,21 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
-import { useHistory } from 'react-router-dom'
 import './ProductCard.css'
 
 function ProductCard({ product }) {
-
-  // console.log(product)
-  const history = useHistory()
-  const handleViewProductDetail = e=>{
-    e.preventDefault()
-    history.push(`/api/products/${product.id}`)
-  }
   return (
-    <div className="k-productcard-container" onClick={handleViewProductDetail}>
-      <div id="product-image-div">
-        <img className='k-productcard-image' src={product.previewImage} alt=''></img>
-      </div>
-      <div id="product-info-div">
-        <h4>{product.name}</h4>
-        <div id="product-rating-div">
-          <p>{product.averageRating}</p>
-          <i class="fa-solid fa-star"></i>
+    <div className="k-productcard-container">
+      <NavLink style={{ textDecoration: "none", color: "black" }} to={`/products/${product.id}`}>
+        <div>
+          <img className='k-productcard-image' src={product.previewImage} alt=''></img>
         </div>
-        <p id='price'>${product.price}</p>
-      </div>
+        <div>
+          <h4>{product.name}</h4>
+          <p>{product.averageRating}</p>
+
+          <p>${product.price}</p>
+        </div>
+      </NavLink>
     </div>
   )
 }
--- react-app/src/components/Product/ProductDetails/index.js
+++ react-app/src/components/Product/ProductDetails/index.js
@@ -2,12 +2,15 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchProductDetail } from '../../../store/product';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import './ProductDetails.css'
 
 function ProductDetails() {
   const { productId } = useParams();
   const dispatch = useDispatch();
   const product = useSelector((state) => state.products.singleProduct)
-  console.log('XXXXXXXXXXXXXXXXXXXXX')
+  console.log('x', product)
 
   useEffect(() => {
     dispatch(fetchProductDetail(productId))
@@ -16,9 +19,23 @@ function ProductDetails() {
   if(!product || Object.keys(product).length === 0) return null;
 
   return(
-    <>
-      {product.name}
-    </>
+    <div className='product-details-container'>
+      <div className='productdetails-carousel-container'>
+        <Carousel useKeyboardArrows={true}>
+        {product.ProductImages.map((product, index) => (
+          <div className='productdetails-image-container'>
+            <img className='productdetails-image' src={product.imageUrl} alt='' key={index}></img>
+          </div>
+        ))}
+        </Carousel>
+      </div>
+        <div className='productdetails-information'>
+            <h4>${product.price}</h4>
+            <h4>{product.name}</h4>
+            <h4>{product.Seller.username}</h4>
+            <h4>{product.description}</h4>
+        </div>
+    </div>
   )
 }
 
