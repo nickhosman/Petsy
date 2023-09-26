46df93fd6e536546a916916b3e3d806cf58b1937
Huynh <huynhlam56@gmail.com>
2023-09-26 00:40:24 -0700

    editing reviews component

---
+++ react-app/src/components/Review/CreateReviews/StarRating.js
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+
+export const StarRating = ({stars, onClickStars}) => {
+  const [hover, setHover] = useState(0);
+  return (
+    <div className='star-rating'>
+      {[...Array(5)].map((star, index) => {
+        index += 1
+        return (
+          <button
+            type='button'
+            key={index}
+            className={index <= (hover || stars) ? 'on' : 'off'}
+            onClick={() => onClickStars(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(stars)}
+          >
+            <span className='star'>
+              <FontAwesomeIcon icon={faStar} />
+            </span>
+          </button>
+        )
+      })}
+    </div>
+  )
+}
