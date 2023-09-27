6d68a0a27eb5e91937faddbf03c7fff193f44569
heehyun1128 <chenyiapp@yahoo.com>
2023-09-26 17:51:35 -0700

    commiting backend search_routes changes

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,24 +1,33 @@
 from flask import Blueprint,request
 from app.models import Category, db
 from flask_login import login_required
-from app.models import Product
-from app.models import Category
+from app.models import Product, Category
+from sqlalchemy import or_
 
 search_routes = Blueprint("search", __name__)
 
 @search_routes.route("/")
 def search():
   q = request.args.get("q")
-
  
-
+  
   if q:
-    result = Product.query.filter(Product.name.ilike(f'%{q}%'), Category.name.ilike(f'%{q}%')).order_by(Category.name.desc()).limit(100).all()
+    print("qqqqqqqqqqqqqqqqqqq",q)
+    # print('Yoooooooooooooooooooooo',Product.query.get(category_id))
+   
+    # result = Product.query.join(Category).filter(Product.name.ilike(f'%{q}%').or_(Category.name.ilike(f'%{q}%'))).limit(100).all()
+   
+    result = Product.query.join(Category).filter(or_(Product.name.ilike(f'%{q}%'),Category.name.ilike(f'%{q}%'))).limit(100).all()
+    print("resultttttttttttttttttttt",result)
+    
+    # print(Category.name)
+    print("xxxxxxxxxxxxxxxxxxxxxx",result)
     search_dict={}
     for product in result:
+      
         data=product.to_dict()
         search_dict[str(product.id)]=data
-    print(search_dict)
+    print("working!!!!!!!!!!",search_dict)
     return {"Search":search_dict}
 
   else:
