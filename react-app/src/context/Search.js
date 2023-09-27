e6a8a4dab97665b3c00830875ffd55045434b351
Nick Hosman <nickhosman@gmail.com>
2023-09-26 20:59:50 -0600

    search context

---
+++ react-app/src/context/Search.js
@@ -0,0 +1,18 @@
+import React, { useState, useContext } from "react";
+
+const SearchContext = React.createContext();
+
+export function SearchProvider({ children }) {
+  const [searchInput, setSearchInput] = useState("");
+
+  const contextValue = {
+    searchInput,
+    setSearchInput,
+  };
+
+  return (
+    <SearchContext.Provider value={contextValue}>{children}</SearchContext.Provider>
+  );
+}
+
+export const useSpotContext = () => useContext(SpotContext);
