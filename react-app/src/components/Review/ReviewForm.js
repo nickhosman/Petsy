547812e38de1677747ab56fbe5822f67a1d33d94
Huynh <huynhlam56@gmail.com>
2023-09-28 20:59:59 -0700

    added css

--- react-app/src/components/Review/ReviewForm.js
+++ react-app/src/components/Review/ReviewForm.js
@@ -4,6 +4,7 @@ import { useModal } from '../../context/Modal'
 import { useState } from "react";
 import { createReviewThunk, fetchAllProducts, getAllReviewsThunk, updateReviewThunk } from "../../store/product";
 import { StarRating } from "./StarRating";
+import './ReviewForm.css'
 
 function ReviewForm({review, formType}) {
   const dispatch = useDispatch()
@@ -18,8 +19,6 @@ function ReviewForm({review, formType}) {
 
   const product = useSelector(state => state.products?.singleProduct)
   const user = useSelector(state => state.session.user)
-  console.log('PRODUCT', product.id)
-  console.log("KJNMKMKMKM", formType)
 
   const handleSubmitReview = async(e) => {
     e.preventDefault()
@@ -46,23 +45,24 @@ function ReviewForm({review, formType}) {
   }
 
   return (
-    <div>
-      <h2>{formType}</h2>
-      <div>
-        <h4>{product.name}</h4>
-        <img src={product.ProductImages[0].imageUrl} style={{ width: '150px'}}/>
+    <div className="review-form">
+      <h2 className="form-title">{formType}</h2>
+      <div className="product-name-img">
+        <h4 className="product-name">{product.name}</h4>
+        <img className='product-img' src={product.ProductImages[0].imageUrl}/>
       </div>
       <form onSubmit={handleSubmitReview}>
-        <div>
-          <h3>Rating:
+        <div className="review-star-rating">
+          <h3 className="rating-star-container">Rating:
             <StarRating
               stars={stars}
               onClickStars={(index) => onClick(index)}
+              className='review-star-buttons'
             />
           </h3>
         </div>
         <div>
-        <p>Please write product review here:</p>
+        <p className="product-review-textbox-header">Please write product review here:</p>
           <textarea
             style={{width: '400px', height: '150px', border: 'solid 1px black', resize: "none"}}
             name='text'
@@ -75,7 +75,9 @@ function ReviewForm({review, formType}) {
           />
         {errors.details && <p id='error-msg'>*{errors.details}</p>}
         </div>
-        <button onSubmit={handleSubmitReview} type='submit'>{formType === 'Create A Review' ? 'Create Review' : 'Update Review'}</button>
+        <div className='update-create-submit-button'>
+          <button onSubmit={handleSubmitReview} type='submit'>{formType === 'Create A Review' ? 'Create Review' : 'Update Review'}</button>
+        </div>
       </form>
     </div>
   )
