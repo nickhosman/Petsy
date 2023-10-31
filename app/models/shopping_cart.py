8e88334d336f722a60661463661240577187f53a
heehyun1128 <chenyiapp@yahoo.com>
2023-10-30 20:06:52 -0700

    shopping cart model

---
+++ app/models/shopping_cart.py
@@ -0,0 +1,22 @@
+# shopping cart
+from .db import db, add_prefix_for_prod, environment, SCHEMA
+
+shopping_carts = db.Table(
+    "shopping_carts",
+
+    db.Column(
+        "user_id",
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod("users.id")),
+        primary_key=True
+    ),
+    db.Column(
+        "product_id",
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod("products.id")),
+        primary_key=True
+    )
+)
+
+if environment == "production":
+    shopping_carts.schema = SCHEMA
