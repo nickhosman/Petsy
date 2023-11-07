4a4c1cd2e07ae1ee75c4747c5eec1336d1c828e4
Nick Hosman <nickhosman@gmail.com>
2023-11-07 15:38:03 -0800

    cart visuals

---
+++ react-app/src/components/Cart/CartDrawer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Drawer from "@mui/material/Drawer";
+
+function CartDrawer() {
+    return (
+        <Drawer anchor="right">
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+        </Drawer>
+    );
+}
+
+export default CartDrawer;
